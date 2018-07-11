9c7f29164b4e28d80f2de7c8a6369ea1bd0ffcee
Luke Rokop <lrokop14@gmail.com>
2018-07-11 17:24:01 +0000

    moved javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,20 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+$(document).ready(function(){
+    console.log("the ship has landed")
+    $("#add-to-do-button").click(function(){
+        console.log("this Works")
+            var NameOfTodo=$("#Add-todo-textbox").val()
+              var lengthOfTodo=$("#add-length-textbox").val()
+            $("ul").append("<li><input type='checkbox'/>"+NameOfTodo+"<span class= 'time-estimate'> "+lengthOfTodo+" minutes</span></li>")
+  
+
+            console.log*(lengthOfTodo)
+        
+    })
+   
+    $("input[type='checkbox']").click(function(){
+        $(this).closest("li").toggleClass("completed-todo")
+    })
+})
